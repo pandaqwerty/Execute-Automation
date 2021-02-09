96ecb1eecd8b94248022613e106b3cd014fba068
Muhammad Fuad <fuad.never@gmail.com>
2021-02-09 21:31:15 +0700

    report

---
+++ Test Suites/Drop_initial.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Drop_initial</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>c0ebd372-639f-466f-be9b-103d65117311</testSuiteGuid>
+   <testCaseLink>
+      <guid>8deb6bb2-de24-4448-a1dc-906600cae5ef</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/dropdownTitle</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0ff87abc-5588-4693-8ef9-60e2c200d345</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Initial text</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
